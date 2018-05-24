e8921a1e1f8664ee020e699e93ed2f910839ac43
Alexandr Sarandi <asarandi@student.42.us.org>
2018-05-24 09:42:20 -0700

    o2 flag for client, re-use parent's mem for srv worker

--- server/src/main.c
+++ server/src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bigfile.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asarandi <asarandi@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 18:48:07 by asarandi          #+#    #+#             */
-/*   Updated: 2018/05/23 19:14:49 by asarandi         ###   ########.fr       */
+/*   Updated: 2018/05/24 09:31:31 by asarandi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 void	server_fork(t_ftp *f)
 {
 	pid_t	pid;
-	t_ftp	*c;
 
 	if ((pid = fork()) == -1)
 		ft_printf(E_FORK);
 	else if (pid == 0)
 	{
-		c = ft_memalloc(sizeof(t_ftp));
-		c = ft_memcpy(c, f, sizeof(t_ftp));
 		close(f->socket);
+		f->pid = getpid();
+		g_ftp = f;
 		(void)signal(SIGINT, worker_sigint_handler);
 		(void)signal(SIGCHLD, worker_sigchld_handler);
-		c->pid = getpid();
 		ft_printf("{cyan}[listener] spawning new worker process, {eoc}");
-		ft_printf("{cyan}pid %lu{eoc}\n", c->pid);
-		g_ftp = c;
-		ftp_send_text(c, 220, FTP_SERVER_NAME);
-		worker_routine(c);
+		ft_printf("{cyan}pid %lu{eoc}\n", f->pid);
+		ftp_send_text(f, 220, FTP_SERVER_NAME);
+		worker_routine(f);
 		exit(0);
 	}
 	close(f->client);
