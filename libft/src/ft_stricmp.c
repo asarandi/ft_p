d0a8c2c961c3488e5a8dd793a3b3aae008e210ea
Alexandr Sarandi <asarandi@student.42.us.org>
2018-05-26 20:39:46 -0700

    fix bug in ft_stricmp(), ft_strnicmp()

--- libft/src/ft_stricmp.c
+++ libft/src/ft_stricmp.c
@@ -6,7 +6,7 @@
 /*   By: asarandi <asarandi@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 15:40:36 by asarandi          #+#    #+#             */
-/*   Updated: 2018/05/19 18:23:12 by asarandi         ###   ########.fr       */
+/*   Updated: 2018/05/26 20:38:57 by asarandi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_stricmp(const char *s1, const char *s2)
 		a = (unsigned char)s1[i];
 		b = (unsigned char)s2[i];
 		if ((((a >= 'a') && (a <= 'z')) || ((a >= 'A') && (a <= 'Z'))) &&
-				(((b >= 'a') && (b <= 'a')) || ((b >= 'A') && (b <= 'Z'))))
+				(((b >= 'a') && (b <= 'z')) || ((b >= 'A') && (b <= 'Z'))))
 		{
 			if (a <= 'Z')
 				a += 'a' - 'A';
